bbe526321779c55a4bbfa3e3ac709bfbf69fefed
rizqitsani <rizqitsani@gmail.com>
2021-05-22 15:57:39 +0700

    feat: add database connection

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+require('dotenv').config();
+
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 // middleware
 app.use(express.static('public'));
@@ -8,6 +12,19 @@ app.use(express.static('public'));
 // view engine
 app.set('view engine', 'ejs');
 
-app.listen(5000);
+// database connection
+const dbURI = process.env.MONGODB_URI;
+
+mongoose
+  .connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    app.listen(PORT);
+    console.log(`Database connected and server running on port ${PORT}`);
+  })
+  .catch();
 
 app.get('/', (req, res) => res.render('home'));
