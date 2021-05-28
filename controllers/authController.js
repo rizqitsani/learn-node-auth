aaaf033655b392aaace7483b448912612b73b5ca
rizqitsani <rizqitsani@gmail.com>
2021-05-28 16:27:34 +0700

    feat: add signup controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,5 @@
+const User = require('../models/User');
+
 const getSignUp = (req, res) => {
   res.render('signup');
 };
@@ -6,8 +8,26 @@ const getLogin = (req, res) => {
   res.render('login');
 };
 
-const postSignUp = (req, res) => {
-  res.send('new signup');
+const postSignUp = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  try {
+    const user = await User.create({ username, email, password });
+
+    res.status(201).json({
+      status: 'success',
+      message: 'User created',
+      data: {
+        user,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      status: 'fail',
+      message: 'Error when creating user',
+    });
+  }
 };
 
 const postLogin = (req, res) => {
